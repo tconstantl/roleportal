9278a981b1944cf3cae61509358428d1dbaad498
tconstantl <thibaut.constantlotz@gmail.com>
2019-06-18 19:09:32 +0900

    add weapon dialog

---
+++ src/app/presentation/weapon-dialog/weapon-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-dialog',
+  templateUrl: './weapon-dialog.component.html',
+  styleUrls: ['./weapon-dialog.component.scss']
+})
+export class WeaponDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
